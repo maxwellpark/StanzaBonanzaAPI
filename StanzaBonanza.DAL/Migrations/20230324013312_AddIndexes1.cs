043b0593a4e5e6f76e449c517dedd69f7da52061
Max <61809127+maxwellpark@users.noreply.github.com>
2023-03-24 13:41:05 +0000

    Add indexes migration

---
+++ StanzaBonanza.DAL/Migrations/20230324013312_AddIndexes1.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace StanzaBonanza.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexes1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Poems_Id",
+                table: "Poems",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Authors_Id",
+                table: "Authors",
+                column: "Id");
+
+            migrationBuilder.InsertData(
+                table: "Poems_Authors",
+                columns: new[] { "Poem_AuthorId", "AuthorId", "PoemId" },
+                values: new object[,]
+                {
+                    { 1, 1, 1 },
+                    { 2, 2, 1 },
+                    { 3, 2, 2 },
+                    { 4, 1, 3 },
+                    { 5, 2, 3 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Poems_Authors",
+                keyColumn: "Poem_AuthorId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Poems_Authors",
+                keyColumn: "Poem_AuthorId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Poems_Authors",
+                keyColumn: "Poem_AuthorId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Poems_Authors",
+                keyColumn: "Poem_AuthorId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Poems_Authors",
+                keyColumn: "Poem_AuthorId",
+                keyValue: 5);
+        }
+    }
+}
